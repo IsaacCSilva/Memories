f0aee50870e1347c347e13d839e0c51cb0fe790c
Daniel <danielkimstudent@hotmail.com>
2017-10-16 13:07:11 -0700

    Created the skeleton of the project with all of the necessary activity screens

---
+++ app/src/main/java/edu/csulb/memoriesapplication/CreateAccountActivity.java
@@ -0,0 +1,8 @@
+package edu.csulb.memoriesapplication;
+
+/**
+ * Created by Danie on 10/16/2017.
+ */
+
+public class CreateAccountActivity {
+}
---
+++ app/src/main/java/edu/csulb/memoriesapplication/MapActivity.java
@@ -0,0 +1,9 @@
+package edu.csulb.memoriesapplication;
+
+/**
+ * Created by Danie on 10/16/2017.
+ */
+
+public class MapActivity {
+
+}
