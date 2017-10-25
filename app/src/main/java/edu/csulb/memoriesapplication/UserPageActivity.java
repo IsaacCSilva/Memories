b0c56cedcb715653f3f6e1ef1c2abd08096aa705
Daniel <danielkimstudent@hotmail.com>
2017-10-25 11:42:05 -0700

    Minor security changes

---
+++ app/src/main/java/edu/csulb/memoriesapplication/UserPageActivity.java
@@ -0,0 +1,48 @@
+package edu.csulb.memoriesapplication;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by Danie on 10/24/2017.
+ */
+
+public class UserPageActivity extends Activity implements View.OnClickListener{
+
+    FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    DatabaseReference databaseReference;
+
+    CircleImageView userImage;
+
+    @Override
+    protected void onCreate(Bundle onSavedInstanceState) {
+        super.onCreate(onSavedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        database = FirebaseDatabase.getInstance();
+
+
+        userImage = (CircleImageView) this.findViewById(R.id.user_profile_picture);
+        userImage.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.user_profile_picture:{
+                //TODO: allow user to go to their gallery and upload a picture
+            }break;
+        }
+    }
+}
