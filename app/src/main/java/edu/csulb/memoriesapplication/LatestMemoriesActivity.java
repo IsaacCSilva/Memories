8ec67722cec0af9cf3fffafb01911279fe09d733
Daniel <danielkimstudent@hotmail.com>
2017-11-21 00:36:39 -0800

    Fixed Isaac's files and pushing action bar, settings, and help activities

--- app/src/main/java/edu/csulb/memoriesapplication/LatestMemoriesActivity.java
+++ app/src/main/java/edu/csulb/memoriesapplication/LatestMemoriesActivity.java
@@ -1,8 +1,11 @@
 package edu.csulb.memoriesapplication;
 
+import android.app.Activity;
+
 /**
  * Created by Danie on 10/16/2017.
  */
 
-public class LatestMemoriesActivity {
+public class LatestMemoriesActivity extends Activity {
+
 }
--- app/src/main/java/edu/csulb/memoriesapplication/TrendingActivity.java
+++ app/src/main/java/edu/csulb/memoriesapplication/TrendingActivity.java
@@ -1,24 +1,25 @@
 package edu.csulb.memoriesapplication;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.transition.Slide;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
-import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 
@@ -26,12 +27,7 @@ import java.util.ArrayList;
  * Created by Danie on 10/16/2017.
  */
 
-public class TrendingActivity extends Activity implements View.OnClickListener{
-
-    private TextView text;
-    private Button button;
-    private FirebaseAuth mAuth;
-    private final String USER_INFO = "user_info";
+public class TrendingActivity extends AppCompatActivity {
 
     //added
     private LinearLayoutManager linearLayoutManager;
@@ -40,6 +36,9 @@ public class TrendingActivity extends Activity implements View.OnClickListener{
     private ArrayList<Polaroid> polaroids;
     private CardViewAdapter rvAdapter;
     private MyConstraintLayout constraintLayout;
+
+    static final int CAM_REQUEST = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,22 +49,22 @@ public class TrendingActivity extends Activity implements View.OnClickListener{
         setTransitions();
 
         //instantiate objects
-        constraintLayout = (MyConstraintLayout)findViewById(R.id.constraintLayout);
+        constraintLayout = (MyConstraintLayout) findViewById(R.id.constraintLayout);
         constraintLayout.setLeftPage(new Intent(this, UserPageActivity.class));
         constraintLayout.setRightPage(new Intent(this, LatestMemoriesActivity.class));
         polaroids = new ArrayList<Polaroid>();
         rvAdapter = new CardViewAdapter(this, polaroids);
-        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         linearLayoutManager = new LinearLayoutManager(this.getApplicationContext());
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(rvAdapter);
         recyclerView.setOnScrollChangeListener(new View.OnScrollChangeListener() {
             @Override
             public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
-                int position1 = ((LinearLayoutManager)recyclerView.getLayoutManager()).findFirstCompletelyVisibleItemPosition();
-                int position2 = ((LinearLayoutManager)recyclerView.getLayoutManager()).findFirstVisibleItemPosition();
-                Log.d("first completely visible item position", ""+position1);
-                if(position1 != -1) {
+                int position1 = ((LinearLayoutManager) recyclerView.getLayoutManager()).findFirstCompletelyVisibleItemPosition();
+                int position2 = ((LinearLayoutManager) recyclerView.getLayoutManager()).findFirstVisibleItemPosition();
+                Log.d("first completely visible item position", "" + position1);
+                if (position1 != -1) {
                     View view = (View) ((LinearLayoutManager) recyclerView.getLayoutManager()).findViewByPosition(position1);
                     if (view instanceof CardView) {
                         Log.d("View is of type", "CardView");
@@ -75,13 +74,13 @@ public class TrendingActivity extends Activity implements View.OnClickListener{
                             videoView.start();
                         }
                     }
-                    if(position2 != -1 && position2 != position1){
-                        Log.d("first visible item postion", ""+position2);
+                    if (position2 != -1 && position2 != position1) {
+                        Log.d("first visible item postion", "" + position2);
                         View view2 = (View) ((LinearLayoutManager) recyclerView.getLayoutManager()).findViewByPosition(position2);
                         View childView = (View) ((CardView) view2).getChildAt(0);
                         if (childView instanceof RelativeLayout) {
                             VideoView videoView = (VideoView) ((RelativeLayout) childView).getChildAt(0);
-                            if(videoView.isPlaying()) {
+                            if (videoView.isPlaying()) {
                                 videoView.pause();
                             }
                         }
@@ -99,21 +98,35 @@ public class TrendingActivity extends Activity implements View.OnClickListener{
         polaroid = new Polaroid(null, uri);
         polaroids.add(polaroid);
         polaroids.add(polaroid);
-
     }
 
     @Override
-    public void onClick(View view) {
-        switch(view.getId()) {
-            case R.id.debug_userpage_button: {
-                Intent intent = new Intent(TrendingActivity.this, UserPageActivity.class);
-                startActivity(intent);
-            }
-            break;
-        }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.user_menu, menu);
+        return true;
     }
 
-    public void setTransitions(){
+    @Override
+    public boolean onOptionsItemSelected(MenuItem items) {
+        switch (items.getItemId()) {
+            case R.id.action_settings: {
+                final Intent intent = new Intent(TrendingActivity.this, Setting.class);
+                startActivity(intent);
+                Toast.makeText(getBaseContext(), "Information: ", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            case R.id.action_cam: {
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(camera_intent, CAM_REQUEST);
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(items);
+    }
+
+
+    public void setTransitions() {
         Slide slide = new Slide();
         slide.setDuration(500);
         slide.setSlideEdge(Gravity.BOTTOM);
